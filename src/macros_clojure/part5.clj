c5c98025f46c86fded4a52c18f2e030173115c49
Cesar Alcancio <cesar.alcancio@gmail.com>
2021-09-25 15:18:32 -0500

    Add part5

--- src/macros_clojure/part5.clj
+++ src/macros_clojure/part5.clj
@@ -1,1 +1,12 @@
 (ns macros-clojure.part5)
+
+; execute only when the number is ímpar
+(odd? 3)
+
+(defmacro
+  when-odd
+  [number & body]
+  `(if (odd? ~number) (do ~@body)))
+
+(when-odd 2 (println "this number is odd") "this number is odd")
+(when-odd 3 (println "this number is odd") "this number is odd")
